a5a4a65354a260d4bef502ba323df49d3ddcd83d
Jayden Richardson <jayrich.dev@gmail.com>
2023-08-29 12:46:16 +0100

    Update Animations.ts

--- src/client/components/threeModules/Animations.ts
+++ src/client/components/threeModules/Animations.ts
@@ -3,15 +3,21 @@
  * It uses the requestAnimationFrame method to create a smooth animation loop.
  */
 export class Animations {
+  lastTime: number = 0;
   /**
    * This method runs the animation loop.
    * It takes a callback function as an argument which is called on each frame.
    * @param callback - A function to be executed each time the frame updates.
    */
-  runAnimation(callback: () => void): void {
+  runAnimation(callback: (deltaTime: number) => void): void {
+    const currentTime = Date.now();
+    const deltaTime = (currentTime - this.lastTime) / 1000.0;
+
     requestAnimationFrame(() => {
       this.runAnimation(callback);
     });
-    callback();
+
+    callback(deltaTime);
+    this.lastTime = currentTime;
   }
 }
